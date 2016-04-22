6066fb961bc8b49385a701ece2c5787b391cf2a9
NiklasEblenkamp <ne018@hdm-stuttgart.de>
2016-04-22 13:24:56 +0200

    Jonas ist kacke

--- TestITP/src/de/hdm/gruppe09/test/shared/testklasse.java
+++ TestITP/src/de/hdm/gruppe09/test/shared/testklasse.java
@@ -5,7 +5,7 @@ public class testklasse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hello!");
-			System.out.println("Chris branch ist scheiße");
+			System.out.println("Jonas branch ist scheiï¿½e");
 	}
 
 }
