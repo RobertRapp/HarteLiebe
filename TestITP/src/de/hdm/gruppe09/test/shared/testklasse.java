31b54c8a8ff02d536bf227f7253a96f4e7d5be0a
RobertRapp <rr033@hdm-stuttgart.de>
2016-04-22 10:57:50 +0200

    erster Commit

---
+++ TestITP/src/de/hdm/gruppe09/test/shared/testklasse.java
@@ -0,0 +1,10 @@
+package de.hdm.gruppe09.test.shared;
+
+public class testklasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello!");
+	}
+
+}
