87a740eae3c65d8857897a9dc81cdfa4e4f93f7e
RobertRapp <rr033@hdm-stuttgart.de>
2016-04-22 12:29:38 +0200

    Hello3

---
+++ ITP_Gruppe09/TEST/src/de/hdm/gruppe09/test/shared/testklasse.java
@@ -0,0 +1,12 @@
+package de.hdm.gruppe09.test.shared;
+
+public class testklasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello!");
+			System.out.println("Hello2");
+			System.out.println("Hello3");
+	}
+
+}
